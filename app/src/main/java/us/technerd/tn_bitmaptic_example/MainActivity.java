6a431a0c65dd5855bd9d9538da3a537589457adb
AmeerHamzaaa <aha1475@gmail.com>
2018-03-07 01:26:11 +0500

    Update Example app

--- app/src/main/java/us/technerd/tn_bitmaptic_example/MainActivity.java
+++ app/src/main/java/us/technerd/tn_bitmaptic_example/MainActivity.java
@@ -1,15 +1,25 @@
 package us.technerd.tn_bitmaptic_example;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapRegionDecoder;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import us.technerd.bitmaptic.Bitmaptic;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView specimen;
+    Button rotate, blur, aspect, pixels;
+    private SeekBar seekBar;
+    Bitmap bitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +28,64 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        specimen = findViewById(R.id.specimen);
+        blur = findViewById(R.id.blur);
+        rotate = findViewById(R.id.rotate);
+        aspect = findViewById(R.id.aspect);
+        pixels = findViewById(R.id.pixels);
+        seekBar = findViewById(R.id.blur_radius);
+
+
+        bitmap = Bitmaptic.getBitmapFromImageView(specimen);
+
+
+        seekBar.setOnSeekBarChangeListener(onSeekBarChanged());
+
+        rotate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                bitmap = Bitmaptic.rotateBitmap(bitmap, 90);
+                specimen.setImageBitmap(bitmap);
             }
         });
+
+        aspect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bitmap = Bitmaptic.scaleToSizeKeepingAspectRatio(bitmap, 1000, true);
+                specimen.setImageBitmap(bitmap);
+            }
+        });
+
+        pixels.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bitmap = Bitmaptic.scaleToPixelQuality(bitmap, 400, 400);
+                specimen.setImageBitmap(bitmap);
+            }
+        });
+
+
+    }
+    private SeekBar.OnSeekBarChangeListener onSeekBarChanged() {
+        return new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float radius = (float) MainActivity.this.seekBar.getProgress();
+                specimen.setImageBitmap(Bitmaptic.blurBitmap(bitmap, MainActivity.this,radius));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
     }
 
     @Override
